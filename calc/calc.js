04975c0133d8afcc93d1bc076ecc0455853cc1de
Bashir Umar Idris <bashtrust15@gmail.com>
2023-04-04 00:46:41 +0100

    Added files to my projects page.

---
+++ calc/calc.js
@@ -0,0 +1,17 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {}
+
+function init () {
+    document.querySelector('.calc-buttons')
+        .addEventListener('click', function(event) {
+            console.log(event);
+            buttonClick(event.target.innerText);
+        })
+}
+
+init();
